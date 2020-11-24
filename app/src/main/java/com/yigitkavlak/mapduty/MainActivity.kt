50771e74eaaf0d59e50b1370f4b9ca7ea0b9661f
yigitkavlak <yigitkavlak97@gmail.com>
2020-11-24 05:15:34 +0300

    First Commit

---
+++ app/src/main/java/com/yigitkavlak/mapduty/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.yigitkavlak.mapduty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/yigitkavlak/mapduty/RegisterActivity.kt
@@ -0,0 +1,79 @@
+package com.yigitkavlak.mapduty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var fauth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        fauth = FirebaseAuth.getInstance()
+
+        val currenUser = fauth.currentUser
+        if(currenUser!=null){
+            val intent = Intent(applicationContext,MapsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun registerClicked(view: View) {
+        val email = userEmailText.text.toString()
+        val password = userPasswordText.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            userEmailText.setError("Email Kısmı Boş Bırakılamaz!")
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            userPasswordText.setError("Password Kısmı Boş Bırakılamaz!")
+            return
+        }
+        if (password.length < 6) {
+            userPasswordText.setError("Password kısmı 6 karakterden az olamaz!")
+            return
+        }
+
+        fauth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(applicationContext, MapsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            if(exception!=null){
+                Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun loginClicked(view: View){
+
+        val email = userEmailText.text.toString()
+        val password = userPasswordText.text.toString()
+
+        fauth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task ->
+
+            if(task.isSuccessful){
+                //SignedIn
+                Toast.makeText(applicationContext,"Hoşgeldiniz: ${fauth.currentUser!!.email.toString()}",Toast.LENGTH_LONG).show()
+                val intent = Intent(applicationContext, MapsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener {exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
