b706b266ccb058553a7f94cf820c85480b9bb339
yigitkavlak <yigitkavlak97@gmail.com>
2020-11-26 05:41:10 +0300

    26kasim marker added

--- app/src/main/java/com/yigitkavlak/mapduty/MapsActivity.kt
+++ app/src/main/java/com/yigitkavlak/mapduty/MapsActivity.kt
@@ -1,37 +1,50 @@
 package com.yigitkavlak.mapduty
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
+import java.lang.Exception
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var fauth : FirebaseAuth
-
+    private lateinit var fauth: FirebaseAuth
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.options_menu,menu)
+        menuInflater.inflate(R.menu.options_menu, menu)
 
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if(item.itemId == R.id.logout){
+        if (item.itemId == R.id.logout) {
             fauth.signOut()
-            val intent = Intent(applicationContext,RegisterActivity::class.java)
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
@@ -48,21 +61,116 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         fauth = FirebaseAuth.getInstance()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(p0: Location) {
+
+                val userLocation = LatLng(p0.latitude, p0.longitude)
+                mMap.addMarker(
+                    MarkerOptions().position(userLocation).title("Konumunuz").draggable(true)
+                )
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+
+                val geocoder = Geocoder(this@MapsActivity, Locale.getDefault())
+
+
+                try {
+                    val addressList = geocoder.getFromLocation(p0.latitude, p0.longitude, 1)
+
+                    if (addressList != null && addressList.isNotEmpty()) {
+                        println(addressList.get(0).toString()) //logcatte görmek için
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+
+            }
+
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        } else {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1,
+                1f,
+                locationListener
+            )
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null) {
+                val lastKnowLatLng = LatLng(lastLocation.longitude, lastLocation.longitude)
+                mMap.addMarker(
+                    MarkerOptions().position(lastKnowLatLng).title("Konumunuz").draggable(true)
+                )
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastKnowLatLng, 15f))
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty()) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        100,
+                        10f,
+                        locationListener
+                    )
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    val myListener = object : GoogleMap.OnMarkerDragListener {
+        override fun onMarkerDragStart(p0: Marker?) {
+
+            val geocoder = Geocoder(this@MapsActivity, Locale.getDefault())
+
+            if (p0 != null) {
+                try {
+
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+
+        }
+
+        override fun onMarkerDrag(p0: Marker?) {
+
+        }
+
+        override fun onMarkerDragEnd(p0: Marker?) {
+
+        }
+
     }
 }
